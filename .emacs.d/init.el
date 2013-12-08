daf51099fdaf25f0aa345bcd91b9b5b6ff794172
Ivanov Sergey <ivanovserg990@gmail.com>
2013-12-08 23:28:53 +0400

    Change color themes; linenum

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,19 +37,37 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
+ '(border ((t nil)))
+ '(column-marker-1 ((t (:background "red"))) t)
+ '(cursor ((t (:background "firebrick1" :foreground "black"))))
+ '(diff-added ((t (:foreground "cyan"))) t)
  '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(font-lock-comment-delimiter-face ((default (:inherit font-lock-comment-face :weight ultra-bold)) (((class color) (min-colors 16)) nil)))
  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
+ '(font-lock-doc-face ((t (:foreground "tomato" :slant italic))))
+ '(font-lock-function-name-face ((t (:foreground "deep sky blue" :underline t :weight bold))))
+ '(font-lock-keyword-face ((t (:foreground "gold" :weight bold))))
+ '(font-lock-string-face ((t (:foreground "tomato" :slant italic))))
+ '(fringe ((nil (:background "black"))))
+ '(fundamental-mode-default ((t (:inherit default))) t)
  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(highlight-current-line-face ((t (:inherit highlight))) t)
  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+ '(lazy-highlight ((t (:background "paleturquoise" :foreground "black"))))
+ '(link ((t (:foreground "DodgerBlue3" :underline t))))
  '(linum ((t (:foreground "black" :weight bold))))
+ '(menu ((t (:background "gray2" :foreground "#FFF991"))))
+ '(minibuffer-prompt ((t (:foreground "royal blue"))))
+ '(mode-line ((t (:background "dark olive green" :foreground "dark blue" :box (:line-width -1 :color "gray75") :weight bold))))
+ '(mode-line-buffer-id ((t (:background "dark olive green" :foreground "beige"))))
+ '(mode-line-highlight ((((class color) (min-colors 88)) nil)))
+ '(mode-line-inactive ((t (:background "dark olive green" :foreground "dark khaki" :weight light))))
+ '(mouse ((t (:background "Grey" :foreground "black"))))
  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil)))
-)
+ '(show-paren-match ((((class color) (background light)) (:background "black"))) t)
+ '(trailing-whitespace ((((class color) (background dark)) (:background "firebrick1"))))
+ '(vertical-border ((t nil))))
 
 (add-to-list 'load-path "~/.emacs.d/auto-complete-1.3.1")
 (require 'auto-complete)
@@ -58,18 +76,19 @@
 (ac-config-default)
 (global-auto-complete-mode t)
 
-(add-to-list 'load-path "~/.emacs.d/fill-column-indicator")
-(require 'fill-column-indicator)
-(define-globalized-minor-mode
- global-fci-mode fci-mode (lambda () (fci-mode 1)))                        
-(global-fci-mode t)
-(setq fci-rule-width 2)
-(setq fci-rule-color "darkblue")
-(setq fci-rule-use-dashes .8)
+; Add good code practice column-indicator. Conflicts with wrapping lines.
+;; (add-to-list 'load-path "~/.emacs.d/fill-column-indicator")
+;; (require 'fill-column-indicator)
+;; (define-globalized-minor-mode
+;;  global-fci-mode fci-mode (lambda () (fci-mode 1)))                        
+;; (global-fci-mode t)
+;; (setq fci-rule-width 2)
+;; (setq fci-rule-color "darkblue")
+;; (setq fci-rule-use-dashes .8)
 
-; initial size of frame
-(add-to-list 'default-frame-alist '(height . 27))
-(add-to-list 'default-frame-alist '(width . 65))
+;; ; initial size of frame
+(add-to-list 'default-frame-alist '(height . 35))
+(add-to-list 'default-frame-alist '(width . 60))
 
 (setq split-height-threshold 80)
 (setq split-width-threshold nil)
@@ -124,6 +143,11 @@
 
 (add-hook 'post-command-hook 'my-flymake-show-help)
 
+(add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
+
+; change font size
+(set-face-attribute 'default nil :height 90)
+
 ;; ------------
 ;; -- Macros --
 ;; ------------
@@ -151,3 +175,89 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+
+;; ---------------------------
+;; -- Aestethitics: Colors, Highlighting, etc. --
+;; ---------------------------
+
+;--------------------;
+;;; User Interface ;;;
+;--------------------;
+
+; always use spaces, not tabs, when indenting
+(setq indent-tabs-mode nil)
+; ignore case when searching
+(setq case-fold-search t)
+; require final newlines in files when they are saved
+(setq require-final-newline t)
+;----------------------;
+;;; Windows & Frames ;;;
+;----------------------;
+
+; language
+(setq current-language-environment "English")
+; don't show the startup screen
+(setq inhibit-startup-screen t)
+; don't show the menu bar
+(menu-bar-mode nil)
+; don't show the tool bar
+(require 'tool-bar)
+(tool-bar-mode nil)
+; don't show the scroll bar
+(scroll-bar-mode nil)
+; number of characters until the fill column
+(setq fill-column 70)
+
+; display line numbers to the right of the window
+(global-linum-mode t)
+; show the current line and column numbers in the stats bar as well
+(line-number-mode t)
+(column-number-mode t)
+
+;------------;
+;;; Cursor ;;;
+;------------;
+; highlight the current line
+(require 'highlight-current-line)
+(global-hl-line-mode t)
+(setq highlight-current-line-globally t)
+(setq highlight-current-line-high-faces nil)
+(setq highlight-current-line-whole-line nil)
+(setq hl-line-face (quote highlight))
+; don't blink the cursor
+(blink-cursor-mode nil)
+; make sure transient mark mode is enabled (it should be by default,
+; but just in case)
+(transient-mark-mode t)
+; turn on mouse wheel support for scrolling
+(require 'mwheel)
+(mouse-wheel-mode t)
+
+;-------------------------;
+;;; Syntax Highlighting ;;;
+;-------------------------;
+; text decoration
+(require 'font-lock)
+(setq font-lock-maximum-decoration t)
+(global-font-lock-mode t)
+(global-hi-lock-mode nil)
+(setq jit-lock-contextually t)
+(setq jit-lock-stealth-verbose t)
+; if there is size information associated with text, change the text
+; size to reflect it
+(size-indication-mode t)
+; highlight parentheses when the cursor is next to them
+(require 'paren)
+(show-paren-mode t)
+
+;; ;-----------------;
+;; ;;; Color Theme ;;;
+;; ;-----------------;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (light-blue))))
