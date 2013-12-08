5d1d496a6eb6c91c5b2e8edef2a5ebc87019f81c
Ivanov Sergey <ivanovserg990@gmail.com>
2013-12-08 22:12:38 +0400

    Create environment for python development: pymacs, rope, ropemacs, python-mode, ropemode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -67,8 +67,62 @@
 (setq fci-rule-color "darkblue")
 (setq fci-rule-use-dashes .8)
 
+; initial size of frame
 (add-to-list 'default-frame-alist '(height . 27))
 (add-to-list 'default-frame-alist '(width . 65))
+
+(setq split-height-threshold 80)
+(setq split-width-threshold nil)
+
+; python-mode
+(setq py-install-directory "~/.emacs.d/python-mode.el-6.1.2")
+(add-to-list 'load-path py-install-directory)
+(require 'python-mode)
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
+(add-to-list 'load-path "~/.emacs.d/pymacs-0.25")
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+;;(eval-after-load "pymacs"
+;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+
+; pymacs
+(add-to-list 'load-path "~/.emacs.d/pymacs-0.25")
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
+
+(require 'pymacs)
+ (pymacs-load "ropemacs" "rope-")
+
+; post flymake errors to mini-buffer
+(defun my-flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+   (let ((help (get-char-property (point) 'help-echo)))
+    (if help (message "%s" help)))))
+
+(add-hook 'post-command-hook 'my-flymake-show-help)
 
 ;; ------------
 ;; -- Macros --
